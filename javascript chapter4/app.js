894433889302d0852cc5c26b975531d920bd478f
muhammadowais <rowais283@gmail.com>
2023-02-06 10:33:15 +0500

    javascript

---
+++ javascript chapter4/app.js
@@ -0,0 +1,10 @@
+var a ="A heaading starting rules for naming JS variable \n <br> <br> "
+document.write(a)
+var b="avriable names can only contain dollar /number / sign /underscore \n <br><br> "
+document.write(b)
+var c ="variable name begin with a small letter / sign /_underscore \n <br><br> "
+document.write(c)
+var d ="Variable names are case sesitive \n <br><br>"
+document.write(d)
+var e =" Variable names should not be JS keywords \n <br><br> "
+document.write(e)
---
+++ javascript chapter5/app.js
@@ -0,0 +1,21 @@
+// var age = prompt("type your age")
+// var isblind = prompt("are you blind")
+// var ishelmet =prompt("you have a helmet")
+// var islicense =prompt("you have a license")
+// if (age >=18 && !isblind && ishelmet && islicense ) {
+    
+// } else {
+    
+// }
+var teamA ="50,20,15,3"
+alert(50+20+15+3)
+
+var teamB ="20.50,9,3"
+alert(20+50+9+3)
+
+if (teamA > teamB ) {
+   document.write("win") 
+    
+} else  {
+    document.write("lose")    
+}
